fc5a36b4a785cd30a80ee8afa278d672f2dbbf27
randompast <matteroflight@gmail.com>
2024-03-29 17:14:09 -0400

    fixed paths from prior testing, effectively reverted some changes

--- Sample_tests/test_imEIC.R
+++ Sample_tests/test_imEIC.R
@@ -1,14 +1,14 @@
 #!/usr/bin/env Rscript
-source(paste("","EIC_MS1_fns.R",  sep=""))
-source(paste("","genEIC.R",       sep=""))
-Rcpp::sourceCpp(paste("","EICim.cpp",sep=""))
+source(paste("","../Scripts/EIC_MS1_fns.R",  sep=""))
+source(paste("","../Scripts/genEIC.R",       sep=""))
+Rcpp::sourceCpp(paste("","../Scripts/EICim.cpp",sep=""))
 
 test_eic_im <- function(){
     args = construct_EM_arguments(
         PrecursorMassAccuracy = 0.01
         ,RT_Tolerances = c(0.1, 0.5)
         ,DT_Tolerances = c(0.1, 3)
-        ,OutputDirectory = "../../../../datafiles/"
+        ,OutputDirectory = "../../../datafiles/"
         ,FeatureID_Cols = c(6,7,12,13)
         ,GroupCSVDirectory = c()
         ,isostring = "13C3;N;S;Cl2;18O;Br2"
@@ -18,7 +18,7 @@ test_eic_im <- function(){
     args$fn_FeatureID = "CM_PFAS_Level6_IM4_bit_DI3_d_DeMP_FIN.csv"
     args$fn_mzxml = "CM PFAS Level 6 IM 4 bit_DI3.d.DeMP.mzML"
     args$fn_eic_output = "EXAMPLE_EIC_IM_OUTPUT_NEW.csv"
-    args$path_to_mzXML_Files = "../../../../datafiles/"
+    args$path_to_mzXML_Files = "../../../datafiles/"
     extract_EICs(args)
 }
 test_eic_im()
--- Sample_tests/test_imMS1.R
+++ Sample_tests/test_imMS1.R
@@ -1,19 +1,19 @@
 #!/usr/bin/env Rscript
-source(paste("","EIC_MS1_fns.R",   sep=""))
-source(paste("","genIsoTable.R",   sep=""))
-source(paste("","MS1Spectragen.R", sep=""))
-Rcpp::sourceCpp(paste("","MS1im.cpp", sep=""))
+source(paste("","../Scripts/EIC_MS1_fns.R",   sep=""))
+source(paste("","../Scripts/genIsoTable.R",   sep=""))
+source(paste("","../Scripts/MS1Spectragen.R", sep=""))
+Rcpp::sourceCpp(paste("","../Scripts/MS1im.cpp", sep=""))
 
 test_MS1 <- function(){
     args = construct_EM_arguments(
         PrecursorMassAccuracy = 0.01
         ,RT_Tolerances = c(0.1, 0.5)
         ,DT_Tolerances = c(0.1, 3)
-        ,OutputDirectory = "../../../../datafiles/"
+        ,OutputDirectory = "../../../datafiles/"
         ,FeatureID_Cols = c(6,7,12,13,4)
         ,GroupCSVDirectory = c()
         ,isostring = "13C3;N;S;Cl2;18O;Br2"
-        ,isotable = "secondary_isotopes.csv"
+        ,isotable = "../Scripts/secondary_isotopes.csv"
         ,isIM = TRUE
     )
     args$fn_FeatureID = "CM_PFAS_Level6_IM4_bit_DI3_d_DeMP_FIN.csv"
@@ -28,7 +28,7 @@ test_MS1 <- function(){
     # args$fn_MS1_output = "EXAMPLE_MS1_OUTPUT.csv"
     for(i in 1:1){
     # for(i in 1:length(fn_mzxmls)){
-        args$path_to_mzXML_Files = if(i == 1) "../../../../datafiles/" else "../../../../datafiles/newones"
+        args$path_to_mzXML_Files = if(i == 1) "../../../datafiles/" else "../../../datafiles/newones"
         args$fn_mzxml = fn_mzxmls[[i]]
         args$fn_MS1_output = paste("MS1_Ion_Mobility_", args$fn_mzxml, "NEW.csv", sep="")
         print(args$fn_mzxml)
